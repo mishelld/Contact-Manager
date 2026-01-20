9b0256453d7d04b40b1625478146ca7485d8eb3a
mishelld <mishelldub@gmail.com>
2026-01-20 15:57:00 +0200

    feat: Integrate UI module and implement initial menu choice handling.

--- contacts.js
+++ contacts.js
@@ -1,6 +1,24 @@
 const manager = require("./contactManager");
+const ui = require("./ui");
 
-
+function handleChoice(choice) {
+  switch (choice) {
+    case "add":
+      break;
+    case "delete":
+      break;
+    case "list":
+      break;
+    case "search":
+      break;
+    case "help":
+      break;
+    default:
+      break;
+  }
+}
 function run() {
+  const choice = ui.getMenuChoice();
+  handleChoice(choice);
 }
 run();
