e9d913ed45ee3fdb8defa3b379afa71067ef36e3
mishelld <mishelldub@gmail.com>
2026-01-20 14:34:29 +0200

    Add loadContacts function

--- contactManager.js
+++ contactManager.js
@@ -1,3 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+const CONTACTS_FILE = path.join(__dirname, "contacts.json");
+
+const loadContacts = function () {
+  try {
+    const data = fs.readFileSync(CONTACTS_FILE, "utf-8"); //"utf-8": bytes -> char
+    const contacts = JSON.parse(data); // string -> object
+    console.log("JSON file Loaded");
+    return contacts;
+  } catch (error) {
+    console.log("Failed to load file contacts");
+    return [];
+  }
+};
+
 const addContact = function (name, email, phone) {};
 
 const deleteContact = function (email) {};
@@ -7,9 +24,9 @@ const searchContact = function (name) {};
 const listContact = function () {};
 
 module.exports = {
-    addContact,
-    deleteContact,
-    searchContact,
-    listContact
-
-}
+  loadContacts,
+  addContact,
+  deleteContact,
+  searchContact,
+  listContact,
+};
